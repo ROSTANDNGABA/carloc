96d50e3653d68532e463aeba80fbd93178318918
ROSTANDNGABA <ROSTANDNGABA@GMAIL.COM>
2026-05-28 14:10:58 +0100

    Fix: Add dj_database_url override for Render database connection

--- gestionlocationbackend/gestionlocationbackend/settings.py
+++ gestionlocationbackend/gestionlocationbackend/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from django.core.exceptions import ImproperlyConfigured
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -255,6 +257,11 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
+
+# --- AJOUTE CE BLOC JUSTE EN DESSOUS ---
+# Si le projet tourne sur Render, on écrase la config avec la variable DATABASE_URL
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
